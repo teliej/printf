ace167f8890d7cac098469d2074c130b8c0d8101
teliej <teliej52@gmail.com>
2023-07-22 05:42:20 -0700

    third

--- _printf.c
+++ _printf.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int _printf(const char *format, ...) {
+int _printf(const char *format, ...)
+{
 	int printed = 0;
 	va_list args;
 	va_start(args, format);
 
 
-	while (*format) {
-		if (*format == '%') {
+	while (*format)
+	{
+		if (*format == '%')
+		{
 			format++;
-			if (*format == '%') {
+			if (*format == '%')
+			{
 				putchar('%');
 				printed++;
-			} else if (*format == 'c') {
+			}
+			else if (*format == 'c')
+			{
 				char c = va_arg(args, int);
 				putchar(c);
 				printed++;
-			} else if (*format == 's') {
+			}
+			else if (*format == 's')
+			{
 				const char* str = va_arg(args, const char*);
-				while (*str) {
+				while (*str)
+				{
 					putchar(*str);
 					printed++;
 					str++;
 				}
 			}
-		} else {
+		}
+		else
+		{
 			putchar(*format);
 			printed++;
 		}
