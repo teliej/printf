e28b4a60efb9d2d1d47e4d9cf1e81ad07fbe7cd4
teliej <teliej52@gmail.com>
2023-07-22 07:31:38 -0700

    first

--- _printf.c
+++ _printf.c
@@ -1,48 +1,66 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include "main.h"
+
+int _choice(const char *format, int *p, va_list args)
+{
+    const char *str;
+    char c;
+
+    switch (*format)
+    {
+        case '%':
+            _putchar('%');
+            p++;
+            break;
+
+        case 's':
+            str = va_arg(args, const char*);
+
+            while (*str)
+            {
+                _putchar(*str);
+                p++;
+                str++;
+            }
+            break;
+
+        case 'c':
+            c = va_arg(args, int);
+
+            _putchar(c);
+            p++;
+            break;
+
+        default:
+            _putchar('%');
+	    p++;
+            break;
+    }
+    return (0);
+}
 
 int _printf(const char *format, ...)
 {
-	int printed = 0;
-	va_list args;
-	va_start(args, format);
+    int printed = 0;
 
+    va_list args;
+    va_start(args, format);
 
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == '%')
-			{
-				putchar('%');
-				printed++;
-			}
-			else if (*format == 'c')
-			{
-				char c = va_arg(args, int);
-				putchar(c);
-				printed++;
-			}
-			else if (*format == 's')
-			{
-				const char* str = va_arg(args, const char*);
-				while (*str)
-				{
-					putchar(*str);
-					printed++;
-					str++;
-				}
-			}
-		}
-		else
-		{
-			putchar(*format);
-			printed++;
-		}
-		format++;
-	}
-
-	va_end(args);
-	return printed;
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            _choice(format, &printed, args);
+        }
+        else
+        {
+            _putchar(*format);
+            printed++;
+        }
+        format++;
+    }
+    va_end(args);
+    return (printed);
 }
